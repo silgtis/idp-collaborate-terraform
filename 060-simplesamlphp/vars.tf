d797c75914676ae1318f7a6940d81ce0d350e96a
Phillip Shipley <phillip.shipley@gmail.com>
2017-06-28 14:13:38 -0400

    adding support for providing a list of trusted ip addresses

--- 060-simplesamlphp/vars.tf
+++ 060-simplesamlphp/vars.tf
@@ -3,7 +3,7 @@ variable "app_env" {
 }
 
 variable "app_name" {
-  default = "simplesamlphp"
+  default     = "simplesamlphp"
   description = "Used in ECS service names and logs, best to leave as default."
 }
 
@@ -19,7 +19,7 @@ variable "cloudflare_token" {}
 variable "cloudflare_domain" {}
 
 variable "db_name" {
-  default = "ssp"
+  default     = "ssp"
   description = "Database name, can be changed but default is fine."
 }
 
@@ -30,7 +30,7 @@ variable "idp_name" {
 variable "logentries_account_key" {}
 
 variable "mysql_user" {
-  default = "ssp"
+  default     = "ssp"
   description = "MySQL username, can be changed but default is fine."
 }
 
@@ -41,25 +41,35 @@ variable "ssp_subdomain" {
 variable "recaptcha_key" {}
 variable "recaptcha_secret" {}
 
+variable "trusted_ip_addresses" {
+  type    = "list"
+  default = []
+}
+
 variable "tf_remote_core" {
   description = "Environment name in Terraform Enterprise for core"
 }
+
 variable "tf_remote_cluster" {
   description = "Environment name in Terraform Enterprise for cluster"
 }
+
 variable "tf_remote_database" {
   description = "Environment name in Terraform Enterprise for database"
 }
+
 variable "tf_remote_elasticache" {
   description = "Environment name in Terraform Enterprise for elasticache"
 }
+
 variable "tf_remote_ecr" {
   description = "Environment name in Terraform Enterprise for ecr"
 }
+
 variable "tf_remote_broker" {
   description = "Environment name in Terraform Enterprise for broker"
 }
+
 variable "tf_remote_pwmanager" {
   description = "Environment name in Terraform Enterprise for pwmanager"
 }
-
